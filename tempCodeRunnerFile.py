1bf1fbd508702edfde5f606fd7095affaa91c50e
Joshua Cuebas <joshcuebas333@aol.com>
2026-04-14 07:54:12 -0400

    acount work and css

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,19 +1,3 @@
-from flask import Flask, render_template, request, redirect, session
-from sqlalchemy import create_engine, text
-from werkzeug.security import generate_password_hash,check_password_hash
-
-app = Flask(__name__)
-app.secret_key = "123"
-
-conn_str = "mysql+pymysql://root:cset155@localhost/online_store"
-engine = create_engine(conn_str, echo=True)
-conn = engine.connect()
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -29,7 +13,7 @@ def login():
             session['username'] = user.username
             session['role'] = user.role
 
-            return redirect('/index')
+            return redirect('/dashboard')
 
         else:
             return render_template('login.html', error="Invalid login")
@@ -72,21 +56,3 @@ def signup():
         return redirect('/signup')
 
     return render_template('signup.html')
-
-
-@app.route('/account', methods=['GET', 'POST'])
-def account():
-
-    return render_template('account.html')
-
-
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect('/')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-    
