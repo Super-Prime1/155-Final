c23a64b45d01894180f9aed7cb80f4cc0a7a9814
Super-Prime1 <superprime1@icloud.com>
2026-04-27 08:08:33 -0400

    fixed the shop wiht discount

--- main.py
+++ main.py
@@ -226,11 +226,16 @@ def shop():
 
     # 4. GET request → load products
     products = conn.execute(text("""
-        SELECT p.*, c.colorname,d.discountprice,d.length
+        SELECT 
+            p.*,
+            c.colorname,
+            d.discountprice,
+            d.length
         FROM products p
         LEFT JOIN color c ON p.colorid = c.colorid
-        LEFT JOIN discount d ON p.productid = d.productid
-        AND CURRENT_DATE <= d.length
+        LEFT JOIN discount_products dp ON p.productid = dp.productid
+        LEFT JOIN discount d ON dp.discountid = d.discountid
+            AND CURRENT_DATE <= d.length
     """)).fetchall()
 
     colors = conn.execute(text("""
